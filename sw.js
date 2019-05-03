f6ac0928708c5887152666bf8fca94fb652d4580
Your Name <you@example.com>
2019-05-03 15:28:33 +0800

    six

--- sw.js
+++ sw.js
@@ -9,9 +9,15 @@ const staticAssets = [
 
 self.addEventListener('install', event => {
     const cache = await cache.open('final-static');
-    cache.addAll(staticAssets);
+    cache.addAll(staticAssets); 
 });
 
 self.addEventListener('fetch', event => {
-    console.log('fetch');
+    const req = event.request;
+    event.respndWith(cacheFirst(req));
 });
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
